0bb0a6f33a91e4db88291611d54dd709cd7c08ca
JeffChristian <jeffchristiansantos@gmail.com>
2022-05-26 17:30:23 -0300

    adicionando conteudo de apresentação com foto pessoal e paragrafos de informação

---
+++ js/toggle.js
@@ -0,0 +1,8 @@
+function toggle(){
+    var sec = document.getElementById('sec');
+    var nav = document.getElementById('navigation');
+    var intro = document.getElementById('introduction-me');
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+    intro.classList.toggle('active');
+}
